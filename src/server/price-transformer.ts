1db2f8f46ac312ce6cecdaba262ea36b8665b4c9
Marcus Chan <marcuscchan@gmail.com>
2024-06-22 22:43:05 -0700

    add info if price/size changes

--- src/server/price-transformer.ts
+++ src/server/price-transformer.ts
@@ -15,3 +15,11 @@ export function transformProductPriceToView<T extends ProductWithPrice>(
   product.price = product.price / 100;
   return product;
 }
+
+export function transformPriceToModel(price: number) {
+  return Math.trunc(price * 100);
+}
+
+export function transformPriceToView(price: number) {
+  return price / 100;
+}
